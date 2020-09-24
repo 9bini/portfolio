2e234f3a977bc17cf575d1bba275f27d62728b6a
9bini <gotn0727@gmail.com>
2020-09-24 14:04:28 +0900

    수료증 열닫기능 추가

---
+++ static/js/imgShow.js
@@ -0,0 +1,24 @@
+function MultiCampus(){
+	$('#MultiCampus').hide().find('.on').removeClass('on'); 
+}
+function Maestro(){
+	$('#Maestro').hide().find('.on').removeClass('on'); 
+}
+function high_class(){
+	$('#high_class').hide().find('.on').removeClass('on');
+}
+function jpa_application2(){
+	$('#jpa_application2').hide().find('.on').removeClass('on');
+}
+function jpa_application(){
+	$('#jpa_application').hide().find('.on').removeClass('on');
+}
+function jpa_basic(){
+	$('#jpa_basic').hide().find('.on').removeClass('on');
+}
+function JPA(){
+	$('#JPA').hide().find('.on').removeClass('on');
+}
+function querydsl(){
+	$('#querydsl').hide().find('.on').removeClass('on');
+}
